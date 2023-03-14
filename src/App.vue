4cc1f2ddeb7bbe3930ca8da0dff9b12674c3b47e
fredsun <fredsun.me@gmail.com>
2023-03-14 21:17:00 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <MyMap mapkey="keyName" class="myMap" />
+</template>
+
+<script>
+import MyMap from './components/my-map.vue';
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyMap
+  }
+}
+</script>
+
+<style scoped>
+.myMap {
+  /* width: 500px;
+  height: 500px; */
+  width: 100vw;
+  height: 100vh;
+}
+
+body,
+html {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
